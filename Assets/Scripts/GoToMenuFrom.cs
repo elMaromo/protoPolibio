337a569fa02ea5e2c1e9e0e0235198da10ea492b
juan <juampats12345@gmail.com>
2024-05-21 17:46:51 +0200

    feat: go back to menu from level selector

---
+++ Assets/Scripts/GoToMenuFrom.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoToMenuFrom : MonoBehaviour
+{
+    public Transform tvButton;
+    public float animationTime;
+    public string menuSceneName;
+
+    // Start is called before the first frame update
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Vector3 buttonPressedPosition = tvButton.position;
+            buttonPressedPosition.z += 0.02f;
+            tvButton.DOMove(buttonPressedPosition, animationTime/2 ).SetLoops(2, LoopType.Yoyo);
+            Invoke("LoadMenuScene", animationTime);
+        }
+    }
+
+    public void LoadMenuScene()
+    {
+        SceneManager.LoadScene(menuSceneName);
+    }
+}
+
+
+
