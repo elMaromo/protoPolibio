8a8d4ab93c90aa31d56fc468dd3cc9c368fa3256
juan <juampats12345@gmail.com>
2024-05-21 20:31:55 +0200

    feat: fix the storm

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,7 +8,6 @@ public class GameManager : MonoBehaviour
     private static GameManager _instance;
     public static GameManager Instance { get { return _instance; } }
 
-    public Transform placeFromTV, placeFarAway, playerCamera;
     public bool startFromTV;
     public float timeToMove;
     public float masterVolume;
@@ -28,19 +27,7 @@ public class GameManager : MonoBehaviour
         DontDestroyOnLoad(gameObject);
     }
 
-    public void Start()
-    {
-        if (startFromTV)
-        {
-            Vector3 posInitPlayer = placeFromTV.position;
-            posInitPlayer += placeFromTV.forward;
-            playerCamera.position = posInitPlayer;
-            playerCamera.rotation = placeFromTV.rotation;
-
-            playerCamera.DOMove(placeFarAway.position, timeToMove);
-            playerCamera.DORotate(placeFarAway.eulerAngles, timeToMove);
-        }
-    }
+    
 
     public void LoadScenewithDelay(string sceneName, float delay)
     {
--- Assets/Scripts/UI/SoundSlider.cs
+++ Assets/Scripts/UI/SoundSlider.cs
@@ -4,5 +4,5 @@ using UnityEngine;
 
 public class SoundSlider : MonoBehaviour
 {
-    
+    public Transform minSoundPos;
 }
