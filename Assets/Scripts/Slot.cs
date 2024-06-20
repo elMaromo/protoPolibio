51a31ecc6fd3e2aa4ca64f6804540f3b9d479888
juan <juampats12345@gmail.com>
2024-06-20 18:48:02 +0200

    feature: level selector reshulon

--- Assets/Scripts/Slot.cs
+++ Assets/Scripts/Slot.cs
@@ -1,21 +1,91 @@
 using System;
+using DG.Tweening;
+using TMPro;
 using UnityEngine;
 
 public class Slot : MonoBehaviour
 {
+    public TextMeshProUGUI textSlot;
     public string levelName;
     private LevelController _levelController;
+    public float amountMoveUp;
+    public float animChangeLevelduration;
+    private bool changingLevels;
+
+    private Vector3 originalPos;
 
     private void Start()
     {
+        changingLevels = false;
         _levelController = FindObjectOfType<LevelController>();
+        originalPos = transform.position;
     }
 
     void OnTriggerEnter2D(Collider2D col)
     {
         if (col.transform.gameObject.CompareTag("Player"))
         {
-            _levelController.OnSlotTriggered(transform,levelName);
+            _levelController.OnSlotTriggered(transform, levelName);
         }
+    }
+
+
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            ChangeToNextLevel();
+        }
+
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            ChangeToPreviousLevel();
+        }
+    }
+
+    public void ChangeToNextLevel()
+    {
+        if (!changingLevels)
+        {
+            changingLevels = true;
+            transform.DOMoveY(amountMoveUp, animChangeLevelduration).SetRelative(true).SetEase(Ease.OutExpo).OnComplete(NextLevel);
+        }
+    }
+
+    public void NextLevel()
+    {
+        changingLevels = false;
+        transform.position = originalPos;
+        int currentLevel = Int32.Parse(textSlot.text);
+        currentLevel++;
+        textSlot.text = currentLevel.ToString();
+        levelName = "Level" + currentLevel.ToString();
+    }
+
+    public void ChangeToPreviousLevel()
+    {
+        if (!changingLevels)
+        {
+            changingLevels = true;
+            transform.DOMoveY(-amountMoveUp, animChangeLevelduration).SetRelative(true).SetEase(Ease.OutExpo).OnComplete(PreviousLevel);
+        }
+    }
+
+    public void PreviousLevel()
+    {
+        changingLevels = false;
+        transform.position = originalPos;
+        int currentLevel = Int32.Parse(textSlot.text);
+        currentLevel--;
+        textSlot.text = currentLevel.ToString();
+        levelName = "Level" + currentLevel.ToString();
+    }
+
+    public string GetLast(string source, int numberOfCharacters)
+    {
+        if (numberOfCharacters >= source.Length)
+            return source;
+        return source.Substring(source.Length - numberOfCharacters);
     }
 }
