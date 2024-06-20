ff1f920af40c4e8acc5d5f14e96172092e92b392
alberto-fuente <afr1004@alu.ubu.es>
2024-06-20 19:51:11 +0200

    Feat: Destroy Ball particles, Fix: TV glass color

--- Assets/Scripts/BaseGame/LevelLimit.cs
+++ Assets/Scripts/BaseGame/LevelLimit.cs
@@ -2,11 +2,14 @@ using UnityEngine;
 
 public class LevelLimit : MonoBehaviour
 {
+    [SerializeField] private ParticleSystem _destroyParticles;
     private void OnTriggerEnter2D(Collider2D other)
     {
         if(other.TryGetComponent<BallControllerV2>(out var ball))
         {
             ball.DestroyBall(false);
+            var particles=Instantiate(_destroyParticles, other.transform.position, Quaternion.identity);
+            Destroy(particles,4);
         }
     }
 }
